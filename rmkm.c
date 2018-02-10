e9ea7ddfe30a20040872cbf53f295d10d32a071d
Cezary Sałbut <csalbut@gmail.com>
2018-02-10 14:30:38 +0100

    Achieve automatic /dev/median node creation & destruction

--- rmkm.c
+++ rmkm.c
@@ -1,5 +1,8 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
 
 /* Kernel Module Boilerplate
  * -------------------------------------------------------------------------- */
@@ -8,13 +11,103 @@ MODULE_AUTHOR("Cezary Salbut");
 MODULE_DESCRIPTION("Rolling Median Kernel Module");
 MODULE_VERSION("0.1");
 
+/* Function Prototypes
+ * -------------------------------------------------------------------------- */
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+
+/* Constants
+ * -------------------------------------------------------------------------- */
+#define SUCCESS 0
+
+// Dev name as it appears in /proc/devices.
+#define DEVICE_NAME "median"
+
+/* Private Data
+ * -------------------------------------------------------------------------- */
+// Descriptor of our character device.
+static struct cdev dev_descr;
+
+ // Device class descriptor for our character device.
+static struct class *dev_class_descr;
+
+// Major/Minor numbers of our char device.
+static dev_t dev_id;
+
+static struct file_operations fops = {
+    .read = device_read,
+    .write = device_write,
+    .open = device_open,
+    .release = device_release,
+};
+
+/* Function Definitions
+ * -------------------------------------------------------------------------- */
 int init_module(void)
 {
     printk(KERN_INFO "RMKM: Hello!\n");
-    return 0;
+
+    if (alloc_chrdev_region(&dev_id, 0, 1, DEVICE_NAME) < 0) {
+        return -1;
+    }
+
+    dev_class_descr = class_create(THIS_MODULE, "chardrv");
+    if (dev_class_descr == NULL) {
+        unregister_chrdev_region(dev_id, 1);
+        return -1;
+    }
+
+    if (device_create(dev_class_descr, NULL, dev_id, NULL, DEVICE_NAME)
+                                                                    == NULL) {
+        class_destroy(dev_class_descr);
+        unregister_chrdev_region(dev_id, 1);
+        return -1;
+    }
+
+    cdev_init(&dev_descr, &fops);
+
+    if (cdev_add(&dev_descr, dev_id, 1) == -1) {
+        device_destroy(dev_class_descr, dev_id);
+        class_destroy(dev_class_descr);
+        unregister_chrdev_region(dev_id, 1);
+        return -1;
+    }
+
+    return SUCCESS;
 }
 
 void cleanup_module(void)
 {
     printk(KERN_INFO "RMKM: Cleaning up.\n");
+    cdev_del(&dev_descr);
+    device_destroy(dev_class_descr, dev_id);
+    class_destroy(dev_class_descr);
+    unregister_chrdev_region(dev_id, 1);
 }
+
+static int
+device_open(struct inode *inode, struct file *file)
+{
+    return SUCCESS;
+}
+
+static int
+device_release(struct inode *inode, struct file *file)
+{
+    return SUCCESS;
+}
+
+static ssize_t
+device_read(struct file *filp, char *buf, size_t len, loff_t *offset)
+{
+    return SUCCESS;
+}
+
+static ssize_t
+device_write(struct file *filp, const char *buf, size_t len, loff_t *offset)
+{
+    return SUCCESS;
+}
+
