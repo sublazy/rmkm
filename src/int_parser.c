88a97674c6275fa1ac00aec53f28e07f6a841906
Cezary Sałbut <csalbut@gmail.com>
2018-02-11 11:35:43 +0100

    Parsing input integers works

--- src/int_parser.c
+++ src/int_parser.c
@@ -1,4 +1,6 @@
+#include <linux/kernel.h>
 #include <linux/ctype.h>
+#include <linux/string.h>
 #include "int_parser.h"
 
 /* Private constants
@@ -10,31 +12,73 @@
 
 /* Private data
  * -------------------------------------------------------------------------- */
-//static char input_buf[MAX_INT_CHARS];
-//static unsigned int idx = 0;
-static char prev_char = 0;
+static char input_buf[MAX_INT_CHARS] = {0};
+static unsigned int idx = 0;
+static int parsed_num = 0;
+static bool new_num_ready = false;
 
 /* Private functions
  * -------------------------------------------------------------------------- */
+static int parse(char *buf)
+{
+    // Null termination required by kstrtoint.
+    buf[MAX_INT_CHARS - 1] = 0;
+
+    unsigned int base = 10;
+    int result = 0;
+    int err = kstrtoint(buf, base, &result);
+
+    // Not really needed, as we only supply verified bytes to the parser.
+    // Also, input from user space should always be valid. So to be extra-safe
+    // let's just print warnings if something isn't right.
+    if (err != 0) {
+        printk(KERN_WARNING "RMKM: error on parsing int: %s\n", buf);
+    }
+
+    return result;
+}
+
+static void parser_reset(void)
+{
+    memset(input_buf, 0, MAX_INT_CHARS);
+    idx = 0;
+    new_num_ready = false;
+}
 
 /* Public functions
  * -------------------------------------------------------------------------- */
 void int_parser_feed(char c)
 {
-    if (isspace(c))
-        return;
+    static char cprev = ' ';
 
-    prev_char = c;
+    // TODO handle negative numbers.
+
+    if ((isdigit(c))) {
+        input_buf[idx] = c;
+        idx++;
+    }
+
+    if (isspace(c) && isdigit(cprev)) {
+        parsed_num = parse(input_buf);
+        new_num_ready = true;
+    }
+
+    cprev = c;
+    // TODO Handle buffer overflows.
 }
 
 bool int_parser_is_num_ready(void)
 {
-    return false;
+    if (new_num_ready)
+        return true;
+    else
+        return false;
 }
 
-// Getting the number resets the parser.
+// Note: Getting the number resets the parser.
 int int_parser_get_num(void)
 {
-    return 0;
+    parser_reset();
+    return parsed_num;
 }
 
